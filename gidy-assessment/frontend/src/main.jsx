547b4860d60eebb42ae1897cede75899f1729d21
Abhishek P <abhishek200228@gmail.com>
2026-03-09 18:01:48 +0530

    Secured Auth0 keys in env file

--- gidy-assessment/frontend/src/main.jsx
+++ gidy-assessment/frontend/src/main.jsx
@@ -1,17 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Auth0Provider } from '@auth0/auth0-react' // 1. Import Auth0 Provider
+import { Auth0Provider } from '@auth0/auth0-react'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* 2. Wrap App with Auth0Provider */}
     <Auth0Provider
-      domain="dev-xjj41n8pztjkvujb.us.auth0.com"       // e.g., "dev-xxxxxx.us.auth0.com"
-      clientId="pqaoV1DU40lT6A9w5Rkm0848Svh8COFy"   // e.g., "aBcdEfGhIjkLmnOpQrStUvWxYz"
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
-        redirect_uri: window.location.origin // Returns the user back to your app after login
+        redirect_uri: window.location.origin
       }}
     >
       <App />
